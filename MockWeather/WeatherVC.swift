e076491a55db7a2572296d2d47a8c41f889b81a9
Bruno Campos <bruno.mcamps@outlook.com>
2018-02-10 15:53:48 -0200

    300 px View almost done - few constrains missing. Testable!

---
+++ MockWeather/WeatherVC.swift
@@ -0,0 +1,19 @@
+//WeatherVC.swift
+//  MockWeather
+//
+//  Created by Bruno Campos on 2/9/18.
+//  Copyright © 2018 Bruno Campos. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
+
